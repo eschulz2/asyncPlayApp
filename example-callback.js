2af7d5a29fcae480e4909fa3331f896de79f72e6
eschulz2 <eschulz2@gmail.com>
2016-02-18 14:20:30 -0600

    getting weather in api response, wow

--- example-callback.js
+++ example-callback.js
@@ -1,1 +1,12 @@
-example-callback.js
+var request = require('request');
+var url = "http://api.openweathermap.org/data/2.5/weather?q=Chicago&units=imperial&appid=44db6a862fba0b067b1930da0d769e98";
+
+request({
+	url: url, json: true
+}, function (error, response, body) {
+  if (error) {
+  	console.log('Unable to fetch weather information.');
+  } else {
+  	console.log(JSON.stringify(body, null, 4));//4 number of spaces
+  }
+});
